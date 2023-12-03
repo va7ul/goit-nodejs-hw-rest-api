8c68202be267fb703c2431792b9acd9f216e87a6
va7ul <vas7ul@gmail.com>
2023-12-03 17:04:47 +0200

    done

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const HttpError = require('./HttpError');
 const ctrlWrapper = require('./ctrlWrapper');
+const sendEmail = require('./sendEmailNodemailer');
 
 module.exports = {
   HttpError,
   ctrlWrapper,
+  sendEmail,
 };
